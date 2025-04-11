6d0ada4756b2fb4325249300eb53d886be62d57b
Vladislav <vladislavrogoza@gmail.com>
2025-04-11 14:53:57 +0200

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "sonner"
 
 export const App = () => {
   return (
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }} basename="/form">
       <Toaster position="bottom-center" richColors closeButton />
       <AppRoutes />
     </BrowserRouter>
